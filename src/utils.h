ae3a4bbba626a405665f24d66b429ae2ddc7c2d7
SuperOxigen <alex@snowycloud.com>
2018-02-20 22:30:35 -0500

    Added a generic utils file for project.

---
+++ src/utils.h
@@ -0,0 +1,62 @@
+/*
+ *  Module: Project Utilities
+ *
+ *  A small set of standard definitions to make the rest of the
+ *  programing much easier.
+ *
+ *  Author: Alex Dale @superoxigen
+ *
+ *  Copyright (c) 2017 Alex Dale
+ *  See LICENSE for information.
+ */
+
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+
+/* Determine if Package is Compiled using C or C++ */
+#if defined(__cplusplus) || defined(__cplusplus__)
+#define _CPLUSPLUS_
+#endif
+
+/* Standard Integer Types */
+#include <stdint.h>
+
+#ifdef _CPLUSPLUS_ /* If C++ */
+
+#define START_C_SECTION extern "C" {
+#define END_C_SECTION };
+
+#ifndef NULL
+#define NULL nullptr
+#endif
+
+#else /* C */
+
+#define START_C_SECTION
+#define END_C_SECTION
+#include <stdbool.h>
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#endif
+
+/* Project standard byte type. */
+typedef uint8_t byte_t;
+
+/* Project Pin Value Type */
+typedef int16_t pin_t;
+
+/* Project Micro Time Value */
+typedef uint32_t time_t;
+
+typedef char char_t;
+
+/* Project String Konstants */
+typedef char_t const * kstring_t;
+
+typedef char_t * string_t;
+
+#endif /* _UTILS_H_ */
