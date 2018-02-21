1d1b370fad7feabe7653c314f682c25860492483
SuperOxigen <alex@snowycloud.com>
2018-02-20 22:27:09 -0500

    Added create gen directory for header generation.

--- scripts/scons_script.py
+++ scripts/scons_script.py
@@ -8,6 +8,10 @@ Import("env")  # noqa
 env = env  # noqa
 
 try:
+    output_dir = os.path.abspath('gen')
+    if not os.path.isdir(output_dir):
+        print("Creating output directory: gen")
+        os.mkdir(output_dir)
     script = os.path.join(os.getcwd(), "scripts/auto_header.py")
     print("Generating config.h")
     call("{} -o gen/config.h .env".format(script), shell=True)
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+#include "config.h"
 
 void setup()
 {
