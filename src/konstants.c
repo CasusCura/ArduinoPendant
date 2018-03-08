26cba265b642dbbd70189a7f3651a87ac35ba548
SuperOxigen <alex@snowycloud.com>
2018-03-07 19:50:19 -0500

    Implemented a string constant definition file.

---
+++ src/konstants.c
@@ -0,0 +1,23 @@
+/*
+ *  Module: Konstants
+ *
+ *  Variable Constants
+ *
+ *  Author: Alex Dale @superoxigen
+ *
+ *  Copyright (c) 2018 Alex Dale
+ *  See LICENSE for information.
+ */
+
+#include "konstants.h"
+
+#include "config.h"
+
+/* Device Information */
+kstring_t kUUID = DEVICE_UUID;
+kstring_t kSerialId = DEVICE_SERIAL;
+
+/* WiFi Information */
+kstring_t kWifiSSID = WIFI_SSID;
+kstring_t kWifiUser = WIFI_USERNAME;
+kstring_t kWifiPass = WIFI_PASSWORD;
---
+++ src/konstants.h
@@ -0,0 +1,34 @@
+/*
+ *  Module: Konstants
+ *
+ *  Variable Constants
+ *
+ *  Author: Alex Dale @superoxigen
+ *
+ *  Copyright (c) 2018 Alex Dale
+ *  See LICENSE for information.
+ */
+
+#ifndef _KONSTANTS_H_
+#define _KONSTANTS_H_
+
+#include "utils.h"
+
+/* String Constants Prototypes */
+
+/*
+ *  Device Information
+ */
+extern kstring_t kUUID;
+extern kstring_t kSerialId;
+
+/*
+ *  WiFi Information
+ */
+extern kstring_t kWifiSSID;
+extern kstring_t kWifiUser;
+extern kstring_t kWifiPass;
+
+
+
+#endif /* _KONSTANTS_H_ */
