fdae1d9b30bbdba2b6e7123501e738c7863c217e
SuperOxigen <alex@snowycloud.com>
2018-03-15 12:44:15 -0400

    Intergrated the interface into the main module.

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,36 @@
  *  See LICENSE for information.
  */
 
-#include <Arduino.h>
-#include "config.h"
+#include "interface.hpp"
+#include "pin_values.h"
+#include "scheduler.h"
+
+#define INTERFACE_LOOP_PERIOD_US    2000
+
+Interface interface(
+    PIN_POWER_LED, PIN_ALERT_LED, PIN_ERROR_LED,
+    PIN_HELP_BUTTON, PIN_CANCEL_BUTTON);
+
+/*
+ *  Interface Loop Task
+ *
+ *  Called periodically to ensure pins are read and LEDs are
+ *  flashed.
+ */
+uint8_t interface_loop_task(void *)
+{
+    interface.loop();
+    return TASK_EXIT_OK;
+}
 
 void setup()
 {
-    // put your setup code here, to run once:
+    scheduler_init();
+    scheduler_periodic_callback(
+        TASK_PRIORITY_LOWEST,
+        INTERFACE_LOOP_PERIOD_US,
+        interface_loop_task,
+        NULL);
 }
 
 void loop()
