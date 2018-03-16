e1b5afa17f251f6383f39a89c631b39ddfe29e8f
SuperOxigen <alex@snowycloud.com>
2018-03-15 20:21:22 -0400

    Implemented the first parts of the WiFi module.

---
+++ src/wifi_driver.cpp
@@ -0,0 +1,105 @@
+/*
+ *  Module: WiFi Driver
+ *
+ *  Wrappers around the poorly documented ESP WiFi module.
+ *
+ *  Author: Alex Dale @superoxigen
+ *
+ *  Copyright (c) 2017 Alex Dale
+ *  See LICENSE for information.
+ */
+
+#include "utils.h"
+
+#define WIFI_ENTEPRISE
+
+/*
+ *  Standard Library
+ */
+
+#include <string.h>
+
+/*
+ * Native Drivers
+ */
+
+#ifdef WIFI_ENTEPRISE
+
+/*
+ *  The following header files are finiky.  The order of import
+ *  matters.  user_interface.h declares type definitions which
+ *  are required by wpa2_enterprise.h.  They also must like to
+ *  a C library.
+ *
+ *  Summary:  Leave these as is.
+ */
+START_C_SECTION
+#include <user_interface.h>
+#include <wpa2_enterprise.h>
+END_C_SECTION
+#endif /* WIFI_ENTEPRISE */
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+/*
+ *  Project Files
+ */
+#include "wifi_driver.h"
+
+/* WiFi credentials. */
+#include "konstants.h"
+
+/*
+ *  Type Definitions
+ */
+
+/*
+ *  Public Module Interface
+ */
+
+C_FUNCTION void wifi_driver_connect(void)
+{
+
+}
+
+C_FUNCTION void wifi_driver_disconnect(void)
+{
+
+}
+
+C_FUNCTION bool_t wifi_driver_is_connected(void)
+{
+
+    return false;
+}
+
+C_FUNCTION void wifi_driver_init(void)
+{
+#ifdef WIFI_ENTEPRISE
+    struct station_config wifi_config;
+    wifi_station_disconnect();
+
+    memset(&wifi_config, 0, sizeof(wifi_config));
+    strcpy((string_t) wifi_config.ssid, kWifiSSID);
+    strcpy((string_t) wifi_config.password, kWifiPass);
+    wifi_station_set_config(&wifi_config);
+
+    /* Enabled Enterprise Authentication. */
+    wifi_station_set_wpa2_enterprise_auth(1);
+
+    wifi_station_set_enterprise_username((byte_t *) kWifiUser, strlen(kWifiUser));
+    wifi_station_set_enterprise_password((byte_t *) kWifiPass, strlen(kWifiPass));
+
+    wifi_station_connect();
+
+#else /* not enterrpise */
+    WiFi.begin(kWifiSSID, kWifiPass);
+#endif
+
+}
+
+C_FUNCTION void wifi_driver_loop(void)
+{
+
+}
