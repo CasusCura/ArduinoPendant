8c3b497ffc0bc37e6456a300f0a7e3ff95d018c6
SuperOxigen <alex@snowycloud.com>
2018-03-21 02:21:18 -0400

    Complete the GET function for HTTPer.

--- src/httper.cpp
+++ src/httper.cpp
@@ -9,67 +9,72 @@
  *  See LICENSE for information.
  */
 
+/* Standard Library */
 #include <string.h>
 
-#include <Base64.h>
+/* 3rd Party Library */
+#include <ESP8266HTTPClient.h>
 
-#include "bufstr.hpp"
+/* Project Library */
+#include "dlog.h"
+#include "smlstr.h"
 #include "konstants.h"
 #include "wifi_driver.h"
 
+/* Self Header */
 #include "httper.hpp"
 
-#define AUTH_BUFFER_LENGTH 256
+#define URL_BUFFER_LENGTH 256
+#define PORT_BUFFER_LENGTH 8
+
 
 /* Konstants */
-
-static kstring_t kGet = "GET";
-static kstring_t kCreate = "CREATE";
-static kstring_t kHttpVersion = "HTTP/1.1";
-static kstring_t kHost = "Host:";
-static kstring_t kAccept = "Accept:";
-static kstring_t kAuth = "Authorization:";
-static kstring_t kCRLF = "\r\n";
-
-static kstring_t kBasicAuth = "Basic";
+static kstring_t kAccept = "Accept";
 static kstring_t kApplicationJson = "application/json";
+static kstring_t kHttp = "http://";
 
-
-static char_t * str_push_str(char_t * start, char_t * pos, kstring_t src, uint16_t length)
+static kstring_t http_code_to_string(int16_t http_code)
 {
-    uint16_t cur;
-    uint16_t i;
-
-    /* Check current length */
-    cur = (uint16_t) (pos - start);
-    if (cur >= (length-1)) return pos;
-
-    for (i = 0; src[i] && (cur+i+1) < length; i++)
+    switch (http_code)
     {
-        *pos++ = src[i];
+        /* 200 series */
+        case HTTP_CODE_OK:
+            return "OK";
+        case HTTP_CODE_CREATED:
+            return "CREATED";
+        case HTTP_CODE_NO_CONTENT:
+            return "No Content";
+        /* 400 series */
+        case HTTP_CODE_BAD_REQUEST:
+            return "Bad Request";
+        case HTTP_CODE_UNAUTHORIZED:
+            return "Unauthorized";
+        case HTTP_CODE_FORBIDDEN:
+            return "Code Forbidden";
+        case HTTP_CODE_NOT_FOUND:
+            return "Not Found";
+        case HTTP_CODE_METHOD_NOT_ALLOWED:
+            return "Method Not Allowed";
+        case HTTP_CODE_PROXY_AUTHENTICATION_REQUIRED:
+            return "Proxy Authentication Required";
+        case HTTP_CODE_REQUEST_TIMEOUT:
+            return "Request Timeout";
+        /* 500 series */
+        case HTTP_CODE_INTERNAL_SERVER_ERROR:
+            return "Internal Server Error";
+        case HTTP_CODE_NOT_IMPLEMENTED:
+            return "Not Implemented";
+        case HTTP_CODE_NETWORK_AUTHENTICATION_REQUIRED:
+            return "Network Authentication Required";
+        default:
+            return "Unknown";
     }
-    *pos = 0;
-
-    return pos;
-}
-
-static char_t * str_push_char(char_t * start, char_t * pos, char_t src, uint16_t length)
-{
-    uint16_t cur;
-
-    cur = (uint16_t) (pos - start);
-    if (cur >= (length-1)) return pos;
-    *pos++ = src;
-    *pos = 0;
-    return pos;
 }
 
 
-char_t HTTPer::request_body[HTTPER_REQUEST_BODY_SIZE];
-char_t HTTPer::response_body[HTTPER_RESPONSE_BODY_SIZE];
-
-HTTPer::HTTPer(kstring_t host, kstring_t path):
+HTTPer::HTTPer(kstring_t host, uint16_t port, kstring_t path):
     _host(host),
+    _port(port),
     _path(path)
 {
     memset(&_parameter_list, 0, sizeof(_parameter_list));
@@ -111,163 +116,187 @@ bool_t HTTPer::remove_parameter(kstring_t key)
     return true;
 }
 
-HTTPer::status_t HTTPer::send_get(void)
+HTTPer::status_t HTTPer::send_get(char_t * payload, uint16_t payload_length)
 {
-    if (!wifi_driver_is_connected()) return STATUS_DISCONNECT;
+    HTTPClient client;
+    String response_body;
+    char_t url_buffer[URL_BUFFER_LENGTH];
+    char_t port_buffer[PORT_BUFFER_LENGTH];
+    BufStr url(url_buffer, URL_BUFFER_LENGTH);
+    int16_t http_code;
+    kstring_t http_code_str;
 
-    clear_request_body();
-    prepare_get();
-    end_request();
-
-    clear_response_body();
-    send_recv();
-
-    return read_status_code();
-}
-
-HTTPer::status_t HTTPer::send_get_expect_json(SafeJson * json_buf)
-{
-    if (!wifi_driver_is_connected()) return STATUS_DISCONNECT;
-
-    clear_request_body();
-    prepare_get();
-    set_accept_type_json();
-    end_request();
-
-    clear_response_body();
-    send_recv();
-
-    if (has_json())
+    /* Check for WiFi connection. */
+    if (!wifi_driver_is_connected())
     {
-        json_buf->parseObject(json_start());
+        return STATUS_DISCONNECT;
     }
 
-    return read_status_code();
-}
+    /*
+     * Prepare URI
+     */
+    DLOG("Preparing GET URL");
 
+    /* Protocol */
+    if (!url.push_str(kHttp))
+    {
+        DLOG_ERR("Could not write protocol to URL");
+        return STATUS_INTERNAL_ERROR;
+    }
+
+    /* Host component */
+    if (!url.push_str(_host))
+    {
+        DLOG_ERR2("Could not write host to URL", _host);
+        return STATUS_INTERNAL_ERROR;
+    }
+
+    /* Port component */
+    if (_port != 80)
+    {
+        url.push_char(':');
+        if (smluintfmt(port_buffer, _port, PORT_BUFFER_LENGTH) >= PORT_BUFFER_LENGTH)
+        {
+            DLOG_ERR("Could not convert port to string.");
+            return STATUS_INTERNAL_ERROR;
+        }
+        if (!url.push_str(port_buffer))
+        {
+            DLOG_ERR2("Could not write port to URL", port_buffer);
+            return STATUS_INTERNAL_ERROR;
+        }
+    }
+
+    /* Path component. */
+    if (!url.push_str(_path))
+    {
+        DLOG_ERR2("Could not write path to URL", _path);
+        return STATUS_INTERNAL_ERROR;
+    }
+
+    /* Parameter component */
+    if (!write_query_parameters(&url))
+    {
+        DLOG_ERR("Could not write query parameters to URL");
+        return STATUS_INTERNAL_ERROR;
+    }
+
+    DLOG2("Beginning HTTP Client", url.buffer());
+
+    if (!client.begin(String(url.buffer())))
+    {
+        DLOG_ERR2("Failed to begin client with URL", url.buffer());
+        return STATUS_INTERNAL_ERROR;
+    }
+
+    /* Set Expected Conent Type */
+    client.addHeader(String(kAccept), String(kApplicationJson));
+
+    /* Set Authorization of Device */
+    client.setAuthorization(kWifiUser, kWifiPass);
+
+    DLOG("Sending GET request...");
+    http_code = client.GET();
+
+    if (http_code < 0)
+    {
+        DLOG_ERR2("HTTP client returned error on GET", _path);
+        return STATUS_INTERNAL_ERROR;
+    }
+
+    http_code_str = http_code_to_string(http_code);
+    DLOG2("GET", http_code_str);
+
+    switch (http_code)
+    {
+        case HTTP_CODE_OK:
+            /* Parse Payload */
+            if (payload)
+            {
+                response_body = client.getString();
+                DLOG2("Got data", response_body.c_str());
+                if (strlcpy(payload, response_body.c_str(), payload_length) >= payload_length)
+                {
+                    DLOG_ERR("Provided payload buffer is too small");
+                    return STATUS_PAYLOAD_TOO_SMALL;
+                }
+                return STATUS_OK;
+            }
+            return STATUS_OK;
+        case HTTP_CODE_NO_CONTENT:
+            if (payload)
+            {
+                DLOG("No data, clearing buffer");
+                memset(payload, 0, payload_length);
+            }
+            return STATUS_OK;
+        case HTTP_CODE_BAD_REQUEST:
+        case HTTP_CODE_METHOD_NOT_ALLOWED:
+            return STATUS_BAD_REQUEST;
+        case HTTP_CODE_UNAUTHORIZED:
+        case HTTP_CODE_FORBIDDEN:
+        case HTTP_CODE_PROXY_AUTHENTICATION_REQUIRED:
+        case HTTP_CODE_NETWORK_AUTHENTICATION_REQUIRED:
+            return STATUS_BAD_AUTH;
+        case HTTP_CODE_INTERNAL_SERVER_ERROR:
+        case HTTP_CODE_NOT_IMPLEMENTED:
+        case HTTP_CODE_REQUEST_TIMEOUT:
+            return STATUS_REMOTE_ERROR;
+        default:
+            return STATUS_UNKNOWN;
+    }
+}
 
 /* Private Methods */
 
-// TODO: check for errors and return internal error codes.
-
-void HTTPer::clear_request_body(void)
+void prepare_parameter(char_t * buffer, uint16_t length)
 {
-    memset(response_body, 0, sizeof(response_body));
+    BufStr parameters(buffer, length);
+
+    if (!buffer || !length) return;
+
 }
 
-void HTTPer::prepare_get(void)
+bool_t HTTPer::write_parameters(BufStr * bstr)
 {
-    BufStr body(request_body, sizeof(request_body));
     uint8_t i;
 
-    body.push_str(kGet);
-    body.push_char(' ');
-    body.push_str(_path);
-
-    if (_parameter_list.n > 0)
+    if (!bstr)
     {
-        body.push_char('?');
-
-        for (i = 0; i < _parameter_list.n; i++)
-        {
-            if (i > 0)
-            {
-                body.push_char('&');
-            }
-            body.push_str(_parameter_list.elems[i].key);
-            body.push_char('=');
-            body.push_str(_parameter_list.elems[i].value);
-        }
+        DLOG_ERR("NULL pointer for BufStr");
+        return false;
     }
 
-    body.push_char(' ');
-    body.push_str(kHttpVersion);
-    body.push_str(kCRLF);
-}
-
-void HTTPer::prepare_create(void)
-{
-    BufStr body(request_body, sizeof(request_body));
-
-    body.push_str(kCreate);
-    body.push_char(' ');
-    body.push_str(_path);
-    body.push_char(' ');
-    body.push_str(kHttpVersion);
-    body.push_str(kCRLF);
-}
-
-void HTTPer::set_host(void)
-{
-    BufStr body(request_body, sizeof(request_body));
-    body.push_str(kHost);
-    body.push_char(' ');
-    body.push_str(_host);
-    body.push_str(kCRLF);
-}
-
-void HTTPer::set_accept_type_json(void)
-{
-    BufStr body(request_body, sizeof(request_body));
-    body.push_str(kAccept);
-    body.push_char(' ');
-    body.push_str(kApplicationJson);
-    body.push_str(kCRLF);
-}
-
-void HTTPer::set_basic_auth(void)
-{
-    BufStr body(request_body, sizeof(request_body));
-    char_t auth_out_buffer[AUTH_BUFFER_LENGTH];
-    char_t auth_in_buffer[AUTH_BUFFER_LENGTH];
-    BufStr auth_in(auth_in_buffer, AUTH_BUFFER_LENGTH);
-
-    memset(auth_out_buffer, 0, AUTH_BUFFER_LENGTH);
-
-    auth_in.push_str(kWifiUser);
-    auth_in.push_char(':');
-    auth_in.push_str(kWifiPass);
-
-    base64_encode(auth_out_buffer, auth_in.buffer(), auth_in.length());
-
-    body.push_str(kAuth);
-    body.push_char(' ');
-    body.push_str(kBasicAuth);
-    body.push_char(' ');
-    body.push_str(auth_out_buffer);
-    body.push_str(kCRLF);
-}
-
-void HTTPer::push_form_data(void)
-{
-    BufStr body(request_body, sizeof(request_body));
-    uint8_t i;
-
-    if (_parameter_list.n > 0)
+    for (i = 0; i < _parameter_list.n; i++)
     {
-        body.push_char('?');
-
-        for (i = 0; i < _parameter_list.n; i++)
+        if (i > 0)
         {
-            if (i > 0)
-            {
-                body.push_char('&');
-            }
-            body.push_str(_parameter_list.elems[i].key);
-            body.push_char('=');
-            body.push_str(_parameter_list.elems[i].value);
+            bstr->push_char('&');
+        }
+        bstr->push_str(_parameter_list.elems[i].key);
+        bstr->push_char('=');
+        if (!bstr->push_str(_parameter_list.elems[i].value))
+        {
+            DLOG_ERR("Failed to add parameter");
+            DLOG_ERR2(" KEY", _parameter_list.elems[i].key);
+            DLOG_ERR2(" VAL", _parameter_list.elems[i].value);
+            return false;
         }
     }
-    body.push_str(kCRLF);
+    return true;
 }
 
-void HTTPer::end_request(void)
+bool_t HTTPer::write_query_parameters(BufStr * url)
 {
-    BufStr body(request_body, sizeof(request_body));
-    body.push_str(kCRLF);
-}
-
-void HTTPer::send_recv(void)
-{
-
+    if (!url)
+    {
+        DLOG_ERR("NULL pointer for BufStr uri");
+        return false;
+    }
+    if (_parameter_list.n > 0)
+    {
+        url->push_char('?');
+        return write_parameters(url);
+    }
+    return true;
 }
--- src/httper.hpp
+++ src/httper.hpp
@@ -12,14 +12,11 @@
 #ifndef _HTTPER_HPP_
 #define _HTTPER_HPP_
 
-#include <ArduinoJson.h>
-
+#include "bufstr.hpp"
 #include "json.hpp"
 #include "utils.h"
 
 #define HTTPER_PARAMETER_MAX    5
-#define HTTPER_REQUEST_BODY_SIZE 1024
-#define HTTPER_RESPONSE_BODY_SIZE 1024
 
 class HTTPer {
 
@@ -35,57 +32,37 @@ class HTTPer {
 
 public:
     typedef enum {
+        /* HTTP Related */
         STATUS_OK,
         STATUS_BAD_AUTH,
-        STATUS_NOT_FOUND,
+        STATUS_BAD_REQUEST,
+        STATUS_REMOTE_ERROR,
 
+        /* System Related */
         STATUS_DISCONNECT,
-        STATUS_UNKNONW
+        STATUS_INTERNAL_ERROR,
+        STATUS_PAYLOAD_TOO_SMALL,
+        STATUS_UNKNOWN
     } status_t;
 
 private:
-
-    static char_t request_body[HTTPER_REQUEST_BODY_SIZE];
-    static char_t response_body[HTTPER_RESPONSE_BODY_SIZE];
-
     parameter_list_t _parameter_list;
     kstring_t _host;
+    uint16_t _port;
     kstring_t _path;
 
 public:
-    HTTPer(kstring_t host, kstring_t path);
+    HTTPer(kstring_t host, uint16_t _port, kstring_t path);
 
     bool_t push_parameter(kstring_t key, kstring_t value);
     bool_t remove_parameter(kstring_t key);
 
-    status_t send_get(void);
-    status_t send_get_expect_json(SafeJson * json_buf);
-    status_t send_create(void);
+    status_t send_get(char_t * payload, uint16_t payload_length);
+    status_t send_create();
 
 private:
-    /*
-     *  Prepare Request Buffer
-     */
-    void clear_request_body(void);
-    void prepare_get(void);
-    void prepare_create(void);
-    void set_host(void);
-    void set_basic_auth(void);
-    void set_accept_type_json(void);
-    void push_form_data(void);
-    void end_request(void);
-
-    void send_recv(void);
-
-    /*
-     *  Read Response Buffer
-     */
-    void clear_response_body();
-    status_t read_status_code(void);
-    char_t * json_start();
-    bool_t has_json();
-
-
+    bool_t write_parameters(BufStr * bstr);
+    bool_t write_query_parameters(BufStr * uri);
 };
 
 #endif /* _HTTPER_HPP_ */
