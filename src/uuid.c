c676a33df35a6d010fb96902353624bf3cc2d5f2
SuperOxigen <alex@snowycloud.com>
2018-03-22 00:55:40 -0400

    Included a UUID library for basic UUID buffer declaration and default values.

---
+++ src/uuid.c
@@ -0,0 +1,56 @@
+/*
+ *  Module: UUID
+ *
+ *  A library for the ID types used in this project.
+ *  The IDs are based on UUIDs.
+ *
+ *  Author: Alex Dale @superoxigen
+ *
+ *  Copyright (c) 2017 Alex Dale
+ *  See LICENSE for information.
+ */
+
+#include "uuid.h"
+
+kstring_t kZeroUUID = "00000000-0000-0000-0000-000000000000";
+
+static bool_t is_hex(char_t c)
+{
+    return (c >= '0' && c <= '9')
+        || (c >= 'a' && c <= 'z')
+        || (c >= 'A' && c <= 'Z');
+}
+
+bool_t uuid_is_uuid(uuid_kref_t uuid)
+{
+    static uint16_t groups[] = {8, 4, 4, 4, 12, 0};
+    uint16_t i, g;
+    char_t const * ptr;
+
+    /* Check for NULL */
+    if (!uuid)
+    {
+        return false;
+    }
+
+    ptr = uuid;
+    for (g = 0; groups[g] > 0; g++)
+    {
+        /* Check for hyphen between groups */
+        if (g != 0 && *ptr++ != '-')
+        {
+            return false;
+        }
+        /* Check each group is hexidecimal values. */
+        for (i = 0; i < groups[g]; i++)
+        {
+            if (!is_hex(*ptr++))
+            {
+                return false;
+            }
+        }
+    }
+
+    /* Check that there are no additional characters */
+    return !*ptr;
+}
---
+++ src/uuid.h
@@ -0,0 +1,33 @@
+/*
+ *  Module: UUID
+ *
+ *  A library for the ID types used in this project.
+ *  The IDs are based on UUIDs.
+ *
+ *  Author: Alex Dale @superoxigen
+ *
+ *  Copyright (c) 2017 Alex Dale
+ *  See LICENSE for information.
+ */
+
+#ifndef _UUID_H_
+#define _UUID_H_
+
+#include "utils.h"
+
+/* 32 Hex Characters, 4 Hyphens, 1 Null Term */
+#define UUID_BUFFER_LENGTH 37
+
+START_C_SECTION
+
+typedef char_t uuid_t[UUID_BUFFER_LENGTH];
+typedef char_t * uuid_ref_t;
+typedef char_t const * uuid_kref_t;
+
+extern kstring_t kZeroUUID;
+
+bool_t uuid_is_uuid(uuid_kref_t uuid);
+
+END_C_SECTION
+
+#endif /* _UUID_H_ */
